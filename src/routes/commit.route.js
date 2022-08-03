7e8ff131246c67f944c610e624e7c6909b23928c
Lervin <lervin100@gmail.com>
2022-08-03 10:04:25 -0400

    Move server to backendfolder

--- src/routes/commit.route.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import * as controller from './controllers/commit.controller.js';
-
-export const router = express.Router();
-
-router.get("/commits", controller.getCommits);
